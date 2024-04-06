b35005eb080f020f3d9da37d18c175c196626c29
Faiez waseem <fazufaiez11@gmail.com>
2024-04-06 15:26:39 +0500

    Ecoaching Query updated and Created Models and Controllers for each

---
+++ app/Http/Controllers/EPlanCourseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EPlanCourse;
+use Illuminate\Http\Request;
+
+class EPlanCourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EPlanCourse $ePlanCourse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EPlanCourse $ePlanCourse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EPlanCourse $ePlanCourse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EPlanCourse $ePlanCourse)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/EPlanPaymentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EPlanPayment;
+use Illuminate\Http\Request;
+
+class EPlanPaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EPlanPayment $ePlanPayment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EPlanPayment $ePlanPayment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EPlanPayment $ePlanPayment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EPlanPayment $ePlanPayment)
+    {
+        //
+    }
+}
