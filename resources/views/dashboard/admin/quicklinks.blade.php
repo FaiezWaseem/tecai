fd2014d9ea339f97fd39f482b88fa379b17c55de
Faiez waseem <fazufaiez11@gmail.com>
2024-09-23 15:41:41 +0500

    fix : QuickLinks Font Size

--- resources/views/dashboard/admin/quicklinks.blade.php
+++ resources/views/dashboard/admin/quicklinks.blade.php
@@ -4,22 +4,16 @@
         <h3 class="card-title">Quick Links</h3>
     </div>
     <div class="row gap-3 p-2">
-        <a href="{{ route('schooladmin.academic.view') }}" class="mx-2" style="text-decoration: underline !important;">View Academics Year</a>
-        <a href="{{ route('schooladmin.students.view') }}" class="mx-2" style="text-decoration: underline !important;">View Students</a>
-        <a href="{{ route('schooladmin.teachers.view') }}" class="mx-2" style="text-decoration: underline !important;" >View Teachers</a>
-        <a href="{{ route('schooladmin.courses.view') }}" class="mx-2" style="text-decoration: underline !important;" >View Courses</a>
-        <a href="{{ route('schooladmin.classes.view') }}" class="mx-2" style="text-decoration: underline !important;" >View Classes</a>
-        <a href="{{ route('schooladmin.notice.board.view') }}" class="mx-2" style="text-decoration: underline !important;" >View Notice Board</a>
-
-
+        <a href="{{ route('schooladmin.academic.view') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;">View Academics Year</a>
+        <a href="{{ route('schooladmin.students.view') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;">View Students</a>
+        <a href="{{ route('schooladmin.teachers.view') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >View Teachers</a>
+        <a href="{{ route('schooladmin.courses.view') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >View Courses</a>
+        <a href="{{ route('schooladmin.classes.view') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >View Classes</a>
+        <a href="{{ route('schooladmin.notice.board.view') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >View Notice Board</a>
+        <a href="{{ route('schooladmin.academic.view') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" > Add New Student</a>
+        <a href="{{ route('schooladmin.teachers.create') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >Add New Teacher</a>
+        <a href="{{ route('schooladmin.courses.create') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >Add New Course</a>
+        <a href="{{ route('schooladmin.classes.create') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >Add New Class</a>
+        <a href="{{ route('schooladmin.notice.board.create') }}" class="mx-1" style="text-decoration: underline !important;font-size: 12px;" >Add New Notice</a>
     </div>
-    <div class="row gap-3 p-2">
-        <a href="{{ route('schooladmin.academic.view') }}" class="mx-2" style="text-decoration: underline !important;" > Add New Student</a>
-        <a href="{{ route('schooladmin.teachers.create') }}" class="mx-2" style="text-decoration: underline !important;" >Add New Teacher</a>
-        <a href="{{ route('schooladmin.courses.create') }}" class="mx-2" style="text-decoration: underline !important;" >Add New Course</a>
-        <a href="{{ route('schooladmin.classes.create') }}" class="mx-2" style="text-decoration: underline !important;" >Add New Class</a>
-        <a href="{{ route('schooladmin.notice.board.create') }}" class="mx-2" style="text-decoration: underline !important;" >Add New Notice</a>
-
-    </div>
-
 </div>
